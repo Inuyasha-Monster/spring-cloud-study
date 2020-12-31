ba3e663cca5138a442120d7984412d8e62617ecd
djlnet <djlnet@17track.cn>
2020-12-31 11:46:48 +0800

    修改：增加payment8001工程

---
+++ cloud-provider-payment8001/src/main/java/com/djl/springcloud/PaymentMain8001.java
@@ -0,0 +1,15 @@
+package com.djl.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author djl
+ * @create 2020/12/31 11:45
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
